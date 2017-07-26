dd274abdd56d51fb50a6a0d27c385d5a6ea10580
huyueb <1062887235@qq.com>
2017-07-26 15:17:28 +0800

    修改样式

--- src/entry/routes/index.js
+++ src/entry/routes/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Router ,Route, hashHistory, IndexRoute} from 'react-router';
 import App from '../layout/App';
-// import Index from 'pages/Test/App';
+import Index from 'pages/Test/App';
 // import Index from 'pages/Test1/App';
-import Index from 'pages/Test2/App';
+// import Index from 'pages/Test2/App';
 
 const Routers = (
   <Router history={hashHistory}>
